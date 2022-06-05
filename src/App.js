acf799d9ff33d3ee052cadcecfb2f6327b8491d8
Vasilis Alexiadis <valexiad@gmail.com>
2022-06-05 21:54:32 +0300

    Start integration with the client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/ProtectedPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <PrivateRoute component={ProtectedPage} path="/protected" exact />
+            <Route component={Login} path="/login" />
+            <Route component={Register} path="/register" />
+            <Route component={Home} path="/" />
+          </Switch>
+        </AuthProvider>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/context/AuthContext.js
@@ -0,0 +1,92 @@
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useHistory } from "react-router-dom";
+import {ApiClient, AuthApi, Login} from 'price_monitoring_service'
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+  let apiClient = new ApiClient()
+  apiClient.basePath = '"http://127.0.0.1:8000/';
+  let apiInstance = new AuthApi(apiClient);
+
+  const [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
+  );
+  const [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? jwt_decode(localStorage.getItem("authTokens"))
+      : null
+  );
+  const [loading, setLoading] = useState(true);
+
+  const history = useHistory();
+
+  const loginUser = async (username, password) => {
+    let body = new Login(username, password);
+
+    apiInstance.authLoginCreate(body, (error, data, response) => {
+      if (error) {
+        console.error(error);
+      } else {
+        setAuthTokens(data);
+        setUser(jwt_decode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+        history.push("/");
+      }
+    });
+  };
+  
+  const registerUser = async (username, password, password2) => {
+    const response = await fetch("http://127.0.0.1:8000/api/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password2
+      })
+    });
+    if (response.status === 201) {
+      history.push("/login");
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
+  const logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+    history.push("/");
+  };
+
+  const contextData = {
+    user,
+    setUser,
+    authTokens,
+    setAuthTokens,
+    registerUser,
+    loginUser,
+    logoutUser
+  };
+
+  useEffect(() => {
+    if (authTokens) {
+      setUser(jwt_decode(authTokens.access));
+    }
+    setLoading(false);
+  }, [authTokens, loading]);
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
